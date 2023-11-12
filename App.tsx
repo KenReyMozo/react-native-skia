afdf6e640a2944dc30e0f5d52c49faed36e6205d
KendallRey <107897863+KendallRey@users.noreply.github.com>
2023-11-13 00:54:20 +0800

    added sample confetti

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@
  *
  * @format
  */
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -12,6 +12,8 @@ import SamplePage from './src/screens/samples/Samples';
 import HomePage from './src/screens/home/Home';
 import PaintingPage from './src/screens/painting/Painting';
 import GroupPage from './src/screens/group/Group';
+import Sample1 from './src/screens/sample/Sample1';
+import {Confetti} from './src/screens/sample/Sample2';
 
 const Stack = createStackNavigator();
 
@@ -23,6 +25,7 @@ function App(): JSX.Element {
         <Stack.Screen name="Home" component={HomePage} />
         <Stack.Screen name="Painting" component={PaintingPage} />
         <Stack.Screen name="Group" component={GroupPage} />
+        <Stack.Screen name="Sample1" component={Confetti} />
       </Stack.Navigator>
     </NavigationContainer>
   );
